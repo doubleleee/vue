f23ca3beccf828c785a91c636c4e3b2eec7af2c3
dabao <jyxb0910@163.com>
2021-03-02 16:30:14 +0800

    finished login

---
+++ 06-shop/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  methods: {
+    // 退出登录
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
